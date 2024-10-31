daef0bda4b3bd3c091455abd8ab737f8e7ae98ff
Guilherme Lemes <guilhermelemes08@gmail.com>
2024-10-30 21:02:11 -0300

    Criação do WebSocket para atualização da pagina de forma automática

--- src/main/java/com/pieropan/propostaapp/listener/PropostaConcluidaListener.java
+++ src/main/java/com/pieropan/propostaapp/listener/PropostaConcluidaListener.java
@@ -1,7 +1,10 @@
 package com.pieropan.propostaapp.listener;
 
+import com.pieropan.propostaapp.dto.PropostaResponseDto;
 import com.pieropan.propostaapp.entity.Proposta;
+import com.pieropan.propostaapp.mapper.PropostaMapper;
 import com.pieropan.propostaapp.repository.PropostaRepository;
+import com.pieropan.propostaapp.service.WebSocketService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -12,8 +15,17 @@ public class PropostaConcluidaListener {
     @Autowired
     private PropostaRepository propostaRepository;
 
+    @Autowired
+    private WebSocketService webSocketService;
+
     @RabbitListener(queues = "${rabbitmq.queue.proposta.concluida}")
     public void propostaConcluida(Proposta proposta) {
-        propostaRepository.save(proposta);
+        atualizarProposta(proposta);
+        PropostaResponseDto propostaResponseDto = PropostaMapper.INSTANCE.convertEntityToDto(proposta);
+        webSocketService.notificar(propostaResponseDto);
+    }
+
+    public void atualizarProposta(Proposta proposta) {
+        propostaRepository.atualizarProposta(proposta.getId(), proposta.getAprovada(), proposta.getObservacao());
     }
 }
--- src/main/java/com/pieropan/propostaapp/repository/PropostaRepository.java
+++ src/main/java/com/pieropan/propostaapp/repository/PropostaRepository.java
@@ -1,6 +1,9 @@
 package com.pieropan.propostaapp.repository;
 
 import com.pieropan.propostaapp.entity.Proposta;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -8,4 +11,9 @@ import java.util.List;
 public interface PropostaRepository extends CrudRepository<Proposta, Long> {
 
     List<Proposta> findAllByIntegradaIsFalse();
+
+    @Transactional
+    @Modifying
+    @Query(value = "UPDATE proposta SET aprovada = :aprovada, observacao = :observacao WHERE id = :id", nativeQuery = true)
+    void atualizarProposta(Long id, boolean aprovada, String observacao);
 }
